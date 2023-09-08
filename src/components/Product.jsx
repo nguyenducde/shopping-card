354c6e218651b9c797c8cce75146912b09129f7b
nguyenducde <ducde678@gmail.com>
2023-09-08 21:51:35 +0700

    db

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,35 +1,36 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import "./css/style.css"
 
-const Info = styled.div`
-  opacity: 0;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  background-color: ${({ theme }) => theme.transparent};
-  z-index: 3;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  transition: all 0.5s ease;
-  cursor: pointer;
-`;
-const Container = styled.article`
-  flex: 1;
-  margin: 5px;
-  min-width: 16rem;
-  height: 20rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: ${({ theme }) => theme.bg};
-  position: relative;
-  &:hover ${Info} {
-    opacity: 1;
-  }
-`;
+// const Info = styled.div`
+//   opacity: 0;
+//   width: 100%;
+//   height: 100%;
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   background-color: ${({ theme }) => theme.transparent};
+//   z-index: 3;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   transition: all 0.5s ease;
+//   cursor: pointer;
+// `;
+// const Container = styled.article`
+//   flex: 1;
+//   margin: 5px;
+//   min-width: 16rem;
+//   height: 20rem;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   background-color: ${({ theme }) => theme.bg};
+//   position: relative;
+//   &:hover ${Info} {
+//     opacity: 1;
+//   }
+// `;
 const LinkDecoration = styled(Link)`
   text-decoration: none;
   color: ${({ theme }) => theme.hover};
@@ -37,21 +38,46 @@ const LinkDecoration = styled(Link)`
 
 const Image = styled.img`
   margin-top: 0.2rem;
-  height: 20rem;
-  width: 18rem;
+  height: 18rem;
+  width: 15rem;
   z-index: 2;
   object-fit: fill;
 `;
 
-const Price = styled.p`
-  font-weight: bold;
-  font-size: 2.4rem;
-  color: white;
-`;
+
+// const Price = styled.p`
+//   font-weight: bold;
+//   font-size: 2.4rem;
+//   color: white;
+// `;
 
 const Product = ({ product }) => {
   return (
-    <Container aria-label={product.name}>
+    // <Container aria-label={product.name}>
+    //   <LinkDecoration
+    //     to={`/product/${product._id}`}
+    //     aria-label={`see more details about ${product.name} $ ${product.price}`}
+    //     role="link"
+    //     title={`${product.name} $ ${product.price} size ${product.size}`}
+    //   >
+    //     <Image
+    //       src={product.imgUrl}
+    //       alt={product.name}
+    //       role="img"
+    //       title={product.imgUrl}
+    //       aria-label={`product-image:${product.description}`}
+    //     />
+    //     <Info role="contentinfo">
+    //       <Price
+    //         role="complementary"
+    //         aria-label={`product price ${product.price}`}
+    //       >
+    //         ${product.price}
+    //       </Price>
+    //     </Info>
+    //   </LinkDecoration>
+    // </Container>
+    <div className="product-cart">
       <LinkDecoration
         to={`/product/${product._id}`}
         aria-label={`see more details about ${product.name} $ ${product.price}`}
@@ -65,16 +91,23 @@ const Product = ({ product }) => {
           title={product.imgUrl}
           aria-label={`product-image:${product.description}`}
         />
-        <Info role="contentinfo">
-          <Price
-            role="complementary"
-            aria-label={`product price ${product.price}`}
-          >
-            ${product.price}
-          </Price>
-        </Info>
+        <span>adidas</span>
+        <h4>{product.name}</h4>
+        <div className="stars">
+          <i className="fa-solid fa-star"></i>
+          <i className="fa-solid fa-star"></i>
+          <i className="fa-solid fa-star"></i>
+          <i className="fa-solid fa-star"></i>
+          <i className="fa-solid fa-star"></i>
+        </div>
+        <div className='info-product'>
+          <h4 className="price">{product.price}</h4>
+          <a href="#">
+            <i className="fa-solid fa-cart-shopping buy-icon"></i>
+          </a>
+        </div>
       </LinkDecoration>
-    </Container>
+    </div>
   );
 };
 
